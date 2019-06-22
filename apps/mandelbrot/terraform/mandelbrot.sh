4fbcf8180a94b294752364da718fb447443670de
Akos Hadnagy <akos.hadnagy@gmail.com>
2019-06-23 01:04:45 +0200

    10th try

--- apps/mandelbrot/terraform/mandelbrot.sh
+++ apps/mandelbrot/terraform/mandelbrot.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-cd /home/centos/src/project_data/fpga-webserver/apps/mandelbrot/build && make launch_nohup &
-exit
+(cd /home/centos/src/project_data/fpga-webserver/apps/mandelbrot/build && make launch)
