812c54d05b9f2f1c9cf00ec85047bebf50d1a8b2
Steve Hoover <steve_git@hooverfam.net>
2019-07-16 11:48:24 -0400

    testing terraform.

--- apps/mandelbrot/terraform/fractalvalley_f1_init.sh
+++ apps/mandelbrot/terraform/fractalvalley_f1_init.sh
@@ -3,4 +3,4 @@
 # Initialization script for fractalvalley F1 server.
 # Compile and start webserver for manual testing at <IP>:80.
 # If all's well, manually rename server to "fractalvalley.net-F1" and this will be the server used by fractalvaalley.net.
-(echo && echo "Running $0" && echo && cd /home/centos/src/project_data/fpga-webserver && source sdaccel_setup && echo && echo "Done SDAccel Setup" && echo && cd apps/mandelbrot/build && make TARGET=hw PORT=80 PREBUILT=true launch_nohup && echo && echo "Done $0" && echo)
+(echo && echo "Running $0" && echo && cd /home/centos/src/project_data/fpga-webserver && source /home/centos/src/project_data/fpga-webserver/sdaccel_setup && echo && echo "Done SDAccel Setup" && echo && cd apps/mandelbrot/build && make TARGET=hw PORT=8888 PREBUILT=true launch_nohup && echo && echo "Done $0" && echo)
