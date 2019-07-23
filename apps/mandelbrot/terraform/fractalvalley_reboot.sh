d9e343c017f411262eb949ae71187bc5c6154923
Steve Hoover <steve_git@hooverfam.net>
2019-07-23 09:49:45 -0400

    Pushing for terraform testing.

--- apps/mandelbrot/terraform/fractalvalley_reboot.sh
+++ apps/mandelbrot/terraform/fractalvalley_reboot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # This script is run by cron @reboot to automatically start the webserver.
-cd /home/centos/src/project_data/fpga-webserver && source ./sdaccel_setup && cd apps/mandelbrot/build && /usr/bin/make TARGET=hw PREBUILT=true live &> make.log &
+# Output is sent to /var/spool/mail/root.
+# Running via ssh establishes the proper environment.
+echo "$0: Output logged in: /home/centos/src/project_data/fpga-webserver/apps/mandelbrot/build/make.log"
+ssh localhost 'cd ~/src/project_data/fpga-webserver && source ./sdaccel_setup && cd apps/mandelbrot/build && (make TARGET=hw PREBUILT=true live &> make.log < /dev/null &)'
