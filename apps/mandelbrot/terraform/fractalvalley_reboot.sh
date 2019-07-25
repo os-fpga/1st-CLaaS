dc4b732edadb70720e650254527be1b6c98ba8e8
Steve Hoover <steve_git@hooverfam.net>
2019-07-25 18:36:30 -0400

    Testing prebuilt F1.

--- apps/mandelbrot/terraform/fractalvalley_reboot.sh
+++ apps/mandelbrot/terraform/fractalvalley_reboot.sh
@@ -5,8 +5,9 @@
 # Running via ssh establishes the proper environment.
 WEBSERVER_LOG='/home/centos/src/project_data/fpga-webserver/apps/mandelbrot/build/webserver.log'
 PASSWORD_ARG=$(if [[ -e "$passwd_file" ]]; then echo PASSWORD=$(cat "$passwd_file"); fi)
+PREBUILT=true
 
 SSH_CMD='cd ~/src/project_data/fpga-webserver && source ./sdaccel_setup && cd apps/mandelbrot/build'
-SSH_CMD="$SSH_CMD && make PREBUILT=false build && echo '$0: Going live with webserver with output in: $WEBSERVER_LOG'"
-SSH_CMD="$SSH_CMD && make PREBUILT=false $PASSWORD_ARG live"
+SSH_CMD="$SSH_CMD && make PREBUILT=$PREBUILT build && echo '$0: Going live with webserver with output in: $WEBSERVER_LOG'"
+SSH_CMD="$SSH_CMD && make PREBUILT=$PREBUILT $PASSWORD_ARG live"
 ssh -o 'StrictHostKeyChecking=no' localhost "$SSH_CMD"
