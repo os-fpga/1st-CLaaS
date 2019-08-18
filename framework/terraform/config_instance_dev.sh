5b6d1c48875b2b73485236510017191bc3f7308b
Steve Hoover <steve_git@hooverfam.net>
2019-08-18 17:45:46 -0400

    WIP

--- framework/terraform/config_instance_dev.sh
+++ framework/terraform/config_instance_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo yum update   # This takes forever, but without it, there is a dependency issue.
+sudo yum -y update   # This takes forever, but without it, there is a dependency issue.
 wget https://s3.amazonaws.com/aws-fpga-developer-ami/1.5.0/Scripts/setup_gui.sh -P /tmp/
 chmod +x /tmp/setup_gui.sh
 source /tmp/setup_gui.sh
