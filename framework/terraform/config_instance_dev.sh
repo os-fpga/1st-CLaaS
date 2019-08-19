96123f064523ffcb0c70bcbeed35d34c78df6187
Steve Hoover <steve_git@hooverfam.net>
2019-08-19 14:15:53 -0400

    Terraform debug.

--- framework/terraform/config_instance_dev.sh
+++ framework/terraform/config_instance_dev.sh
@@ -4,5 +4,5 @@
 # Hoping this resolves itself soon enough.
 #wget https://s3.amazonaws.com/aws-fpga-developer-ami/1.5.0/Scripts/setup_gui.sh -P /tmp/
 #chmod +x /tmp/setup_gui.sh
-sed -i 's|^setup_password$|#setup_password|' /tmp/setup_gui.sh
-/tmp/setup_gui.sh
+#sed -i 's|^setup_password$|#setup_password|' /tmp/setup_gui.sh
+#/tmp/setup_gui.sh
