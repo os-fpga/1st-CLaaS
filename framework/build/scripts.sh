418b6387f4817b45ebbc3da78ef68d12598dee02
Cloud User <centos@ip-172-31-23-12.ec2.internal>
2021-08-19 15:27:58 +0000

    remove launch_waveform

--- framework/build/scripts.sh
+++
@@ -1,2 +0,0 @@
-export PATH=$PATH:/home/centos/src/project_data/aws-fpga/shared/bin/scripts:/opt/Xilinx/SDx/2018.3.op2405991/bin:/opt/Xilinx/Vivado/2018.3.op2405991/bin:/usr/local/Modules/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/
-sbin:/srv/git/centos-git-common:/home/centos/.local/bin:/home/centos/bin:/srv/git/centos-git-common:/home/centos/.local/bin:/home/centos/bin
