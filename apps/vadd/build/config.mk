e617717019d3e9a79db7b4f067d0cb2dfaed48f6
Steve Hoover <steve_git@hooverfam.net>
2022-10-27 10:48:55 -0400

    Revert "int"
    This reverts commit 6ce503fa995cc3402789c22924cf82d98904a39d.

--- apps/vadd/build/config.mk
+++ apps/vadd/build/config.mk
@@ -1,5 +1,5 @@
 VIVADO := $(XILINX_VIVADO)/bin/vivado
 $(TEMP_DIR)/vadd.xo: $(ABS_COMMON_REPO)/apps/vadd/fpga/scripts/package_kernel.tcl $(ABS_COMMON_REPO)/apps/vadd/fpga/scripts/gen_xo.tcl $(ABS_COMMON_REPO)/apps/vadd/fpga/src/*.sv $(ABS_COMMON_REPO)/apps/vadd/fpga/src/*.v $(ABS_COMMON_REPO)/apps/vadd/fpga/src/tlv/*.tlv
-	sandpiper-saas -i $(ABS_COMMON_REPO)/apps/vadd/fpga/src/tlv/sw.tlv --outdir $(ABS_COMMON_REPO)/apps/vadd/fpga/src/ -o krnl_vadd_rtl_adder.sv --fmtFlatSignals --bestsv --noline
+	sandpiper-saas -i $(ABS_COMMON_REPO)/apps/vadd/fpga/tlv/sw.tlv --outdir $(ABS_COMMON_REPO)/apps/vadd/fpga/ -o krnl_vadd_rtl_adder.sv --fmtFlatSignals --bestsv --noline
 	mkdir -p $(TEMP_DIR)
-	$(VIVADO) -mode batch -source $(ABS_COMMON_REPO)/apps/vadd/fpga/scripts/gen_xo.tcl -tclargs $(TEMP_DIR)/vadd.xo vadd $(TARGET) $(DEVICE) $(XSA)
+	$(VIVADO) -mode batch -source $(ABS_COMMON_REPO)/apps/vadd/fpga/gen_xo.tcl -tclargs $(TEMP_DIR)/vadd.xo vadd $(TARGET) $(DEVICE) $(XSA)
